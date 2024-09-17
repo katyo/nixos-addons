71c7af33c4d6db9fb1a00b3e2b3d2dc095629c9c
K. <kayo@illumium.org>
2024-09-17 11:08:14 +0500

    Add blueman package locale fix

--- packages/default.nix
+++ packages/default.nix
@@ -8,5 +8,15 @@ self: super: with self; {
 
   victoriametrics-datasource-bin = grafanaPlugins.callPackage ./grafana-plugins/victoriametrics-datasource/binary.nix {};
 
+  blueman-fixed = super.blueman.overrideDerivation (attrs: {
+    postInstall = ''
+      cat >> $out/lib/python*/site-packages/blueman/Constants.py << EOS
+
+      import gettext
+      gettext.bindtextdomain("blueman", LOCALEDIR)
+      EOS
+    '';
+  });
+
   russian-trusted-ca = callPackage ./extra-certs/russian-trusted-ca.nix {};
 }
