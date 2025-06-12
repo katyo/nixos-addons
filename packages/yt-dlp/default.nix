a0efb4cdff3885c4fc716efa877c9c505049f7b2
K. <kayo@illumium.org>
2025-06-12 22:33:38 +0300

    Update yt-dlp

--- packages/yt-dlp/default.nix
+++ packages/yt-dlp/default.nix
@@ -17,13 +17,13 @@ python3Packages.buildPythonApplication rec {
   # The websites yt-dlp deals with are a very moving target. That means that
   # downloads break constantly. Because of that, updates should always be backported
   # to the latest stable release.
-  version = "2024.12.6";
-  pyproject = true;
+  version = "2025.6.9";
+  format = "pyproject";
 
   src = fetchPypi {
     inherit version;
     pname = "yt_dlp";
-    hash = "sha256-dD2+CB6ocb4/X/CD4s2V2oZt6nc/xwrmsQmDjPv3KsQ=";
+    hash = "sha256-dR9To7YTU1Ir+AX6MLvL0WZmEmU345cG6rT4w2jxEaw=";
   };
 
   build-system = with python3Packages; [
