e64ba3cdd0dce43e72cd2382110d5279e0ec9497
K. <kayo@illumium.org>
2025-09-24 14:17:34 +0500

    Update comfyui wd14-tagger

--- packages/comfyui/wd14-tagger/default.nix
+++ packages/comfyui/wd14-tagger/default.nix
@@ -3,11 +3,11 @@
 let
     owner = "pythongosssss";
     repo = "ComfyUI-WD14-Tagger";
-    rev = "763d833";
-    hash = "sha256-nHYd4RdiN3dSyYW+zuf7cOqtvZdp0qu142KE8Lh4r+E=";
+    rev = "9e0a6e7";
+    hash = "sha256-ww3KpXR5gpDdVKWipYMtqkCTh5iLpqLaenGto0Z28WQ=";
 
     pname = lib.strings.toLower repo;
-    version = "1.0.0-git${rev}";
+    version = "1.0.1-git${rev}";
 
 in buildCustomNode {
     inherit pname version;
